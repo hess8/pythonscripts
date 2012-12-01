3da2a903aab23ea54bcc7ca8e012acafe5b76f4e
Bret Hess <hess.byu@gmail.com>
2012-11-30 22:33:18 -0700

    misc, isolatedrun

--- pythonscripts_gr/half_graphane_scripts/IsolatedRun.py
+++ pythonscripts_gr/half_graphane_scripts/IsolatedRun.py
@@ -6,27 +6,27 @@ mainDir = "/bluehome/bch/vasprun/graphene.structures/half_graphane/"
 potcardir = "/bluehome/bch/hessgroup/vaspfiles/src/potpaw_PBE/"
 
 #Specify the type of run
-runType = ['relax']
+runType = ['isolated']
 
 #Specify the name of the type of run
 runName = "relaxation"
 
 #Specify a Poscar file
-poscar = mainDir + 'poscar/relax.poscar'
+poscar = mainDir + 'poscar/isolatedrun.poscar'
 
 #Specify a KPoints file
-kpoints = mainDir + 'kpoints/grapheneionicrun.kpoints'
+kpoints = mainDir + 'kpoints/isolatedrun.kpoints'
 
 #Specify an Incar file
-incar = mainDir + 'incar/grapheneionicrun.incar'
+incar = mainDir + 'incar/isolatedrun.incar'
 
 #Specify a Potcar file
-potcar = mainDir + 'potcar/graphene.potcar'
+potcar = mainDir + 'potcar/isolatedrun.potcar'
 
 #Specify Poscar variables
 poscarVariables = {
 '@distance':
-	[8,6,4,3,2,1]
+	[0]
 }
 
 #Specify KPoints variables
@@ -50,28 +50,35 @@ incarVariables = {
 #	['Hf']
 #}
 
+#elementList = {
+#'@adatom':
+#[
+#"Ac", "Ca_sv", "Eu", "H.75", "Lu_3", "Np", "Pr_3", "Si_h","Tm",
+#"Ac_s", "Cd", "Eu_2", "He", "Mg", "Np_s", "Pt", "Sm", "Tm_3",
+#"Ag", "Ce", "F", "Hf", "Mg_pv", "N_s", "Pu", "Sm_3", "U",
+#"Al", "Ce_3", "Fe", "Hf_pv", "Mn", "O", "Pu_s", "Sn", "U_s",
+#"Al_h", "C_h", "Fe_pv", "Hg", "Mn_pv", "O_h", "Rb_pv", "Sn_d", "V",
+#"Ar", "Cl", "F_h", "H_h", "Mo", "Os", "Rb_sv", "Sr_sv", "V_pv",
+#"As", "Cl_h", "F_s", "Ho_3", "Mo_pv", "O_s", "Re", "Ta", "V_sv",
+#"Au", "Co", "Ga", "I", "N", "Os_pv", "Ta_pv", "W",
+#"B", "Cr", "Ga_d", "In", "Na", "P", "Re_pv", "Tb_3", "W_pv",
+#"Ba_sv", "Cr_pv", "Ga_h", "In_d", "Na_pv", "Pa", "Rh", "Tc", "Xe",
+#"Be", "C_s", "Gd", "Ir", "Na_sv", "Pa_s", "Rh_pv", "Tc_pv", "Yb",
+#"Be_sv", "Cs_sv", "Gd_3", "K_pv", "Nb_pv", "Pb", "Ru", "Te", "Yb_2",
+#"B_h", "Cu", "Ge", "Kr", "Nb_sv", "Pb_d", "Ru_pv", "Th", "Y_sv",
+#"Bi", "Cu_pv", "Ge_d", "K_sv", "Nd", "Pd", "S", "Th_s", "Zn",
+#"Bi_d", "Ge_h", "La", "Nd_3", "Pd_pv", "Sb", "Ti", "Zr",
+#"Br", "H", "La_s", "Ne", "P_h", "Sc_sv", "Ti_pv", "Zr_sv",
+#"B_s", "Dy_3", "H1.25", "Li", "N_h", "Pm", "Se", "Ti_sv",
+#"C", "Er_2", "H1.5", "Li_sv", "Ni", "Pm_3", "S_h", "Tl",
+#"Ca_pv", "Er_3", "H.5", "Lu", "Ni_pv", "Pr", "Si", "Tl_d"
+#]
+#}
+
 elementList = {
 '@adatom':
 [
-"Ac", "Ca_sv", "Eu", "H.75", "Lu_3", "Np", "Pr_3", "Si_h","Tm",
-"Ac_s", "Cd", "Eu_2", "He", "Mg", "Np_s", "Pt", "Sm", "Tm_3",
-"Ag", "Ce", "F", "Hf", "Mg_pv", "N_s", "Pu", "Sm_3", "U",
-"Al", "Ce_3", "Fe", "Hf_pv", "Mn", "O", "Pu_s", "Sn", "U_s",
-"Al_h", "C_h", "Fe_pv", "Hg", "Mn_pv", "O_h", "Rb_pv", "Sn_d", "V",
-"Ar", "Cl", "F_h", "H_h", "Mo", "Os", "Rb_sv", "Sr_sv", "V_pv",
-"As", "Cl_h", "F_s", "Ho_3", "Mo_pv", "O_s", "Re", "Ta", "V_sv",
-"Au", "Co", "Ga", "I", "N", "Os_pv", "Ta_pv", "W",
-"B", "Cr", "Ga_d", "In", "Na", "P", "Re_pv", "Tb_3", "W_pv",
-"Ba_sv", "Cr_pv", "Ga_h", "In_d", "Na_pv", "Pa", "Rh", "Tc", "Xe",
-"Be", "C_s", "Gd", "Ir", "Na_sv", "Pa_s", "Rh_pv", "Tc_pv", "Yb",
-"Be_sv", "Cs_sv", "Gd_3", "K_pv", "Nb_pv", "Pb", "Ru", "Te", "Yb_2",
-"B_h", "Cu", "Ge", "Kr", "Nb_sv", "Pb_d", "Ru_pv", "Th", "Y_sv",
-"Bi", "Cu_pv", "Ge_d", "K_sv", "Nd", "Pd", "S", "Th_s", "Zn",
-"Bi_d", "Ge_h", "La", "Nd_3", "Pd_pv", "Sb", "Ti", "Zr",
-"Br", "H", "La_s", "Ne", "P_h", "Sc_sv", "Ti_pv", "Zr_sv",
-"B_s", "Dy_3", "H1.25", "Li", "N_h", "Pm", "Se", "Ti_sv",
-"C", "Er_2", "H1.5", "Li_sv", "Ni", "Pm_3", "S_h", "Tl",
-"Ca_pv", "Er_3", "H.5", "Lu", "Ni_pv", "Pr", "Si", "Tl_d"
+"Ac", "Ca_sv"
 ]
 }
 import ScriptTools
@@ -81,20 +88,16 @@ tools = ScriptTools.VaspTools(mainDir,runName,runType,poscar,kpoints,incar,
 
 tools.BuildNewRun()
 
-print "Done"
-raw_input()
+print "Done with creating folders"
+raw_input() #Pause for response
 
 
 
 
-#script.py
 import os,subprocess,time,sys, shutil
-#mainDir = "/bluehome/bch/TransitionMetals/"
-run = runName
+
 newRun = "DOS"
 newRunFile = "DOSCAR" #Will check to see if higher level is already done, then skip it
-#kpoints = "Automatic mesh\n0\nGamma\n33 33 1\n0 0 0"
-#incar = "ENCUT=500\nPREC=Accurate\nEDIFF=1E-6\nISMEAR=-5\nSIGMA=0.12\nISPIN=2\nLORBIT=10\n"
 copyFiles = True
 
 global toCheckList
@@ -112,8 +115,9 @@ def checkFolders():
     for path in toCheckList:
         print("CHECK NEXT LINE")
         print(path.split("/")[-2])
-        if path.split("/")[-2] == run:
+        if path.split("/")[-2] == runName:
             checkedList.append(path)
+            toRunList.append(path)
             
 def checkForNewRun():
     for path in checkedList:
@@ -130,28 +134,30 @@ def checkForNewRun():
             toRunList.append(parpath+"/")
 
 print("\nInitializing...\n")
-print("Finding Directories to do %s on\n" % run)
-print("Searching all Directories in " + mainDir+"\n")
+print("Finding Directories to do %s on\n" % runName)
+dir = mainDir + runType[0] + "/"
+print("Searching all Directories in " + dir+"\n")
 toCheckList = []
 checkedList = []
 toRunList = []
-addToList(mainDir)
+addToList(dir)
 
-print("Checking to see which folders contain "+run+"\n")
+print("Checking to see which folders contain "+runName+"\n")
 time.sleep(1)
 checkFolders()
 
-print("Checking to see if any folders have already converged\n")
-time.sleep(1)
-checkForNewRun()
+#print("Checking to see if any folders have already converged\n")
+#time.sleep(1)
+#checkForNewRun()
 
+print "\nThe following folders are in checkedFolder:"
+for i in toCheckList:
+    print("toCheckList contains : " + i)
 print "\nThe following folders are in checkedList:"
 for i in checkedList:
     print("checkedList contains : " + i)
     
-print "\nThe following folders are in checkedFolder:"
-for i in toCheckList:
-    print("toCheckList contains : " + i)
+
 print "\nThe following folders will be run:"
 for i in toRunList:
     print("toRunList contains : " + i)
@@ -160,7 +166,7 @@ for i in toRunList:
 
 print"\n"
 for folder in toRunList:
-    newFolder = folder+run+"/"
+    newFolder = folder
     #print "Copying " + previousRun + " to " + newRun + " in folder " + folder
     #command = subprocess.check_call(['cp','-r',folder+previousRun,newFolder])
     #print "Copying CONTCAR to POSCAR"
@@ -186,6 +192,5 @@ for folder in toRunList:
         output = proc.stdout.readline()
         file.write(output)
         print output,folder
-    file.close()
-    
-
+    file.close()   
+print "Done with submitting jobs"
