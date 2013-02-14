ef44ab4b718effd79c6ab71abc038b9a8e0f28c8
Bret Hess <hess.byu@gmail.com>
2013-02-14 13:11:08 -0700

    ce scripts running

--- cluster_expansion/run_scripts/ceBasisTestRun.py
+++ cluster_expansion/run_scripts/ceBasisTestRun.py
@@ -1,7 +1,7 @@
 ''' For isolated atoms '''
 ################## Directories ################## 
 #Specify Directories to use
-mainDir = "/fslhome/bch/cluster_expansion/cluster_size_test/agpt/"
+mainDir = '/fslhome/bch/cluster_expansion/cluster_size_test/agpt/'
 # input files directory
 a = mainDir.split('/')
 del a[-2]  #removes last part of path 
@@ -10,10 +10,13 @@ inputDir = '/'.join(a) + 'input/'
 
 #Specify the type of run
 runtype = 'ncl_ntr'
-#runtype = ['test']
+
 
 #Specify the name of the type of run
-runname = 'run_10_15' #make clusters
+uncleParam = [10,15]
+runname = 'run'
+for par in uncleParam:
+    runname += '_%s' % par
 
 #Specify a lat.in file
 latInFile = inputDir + 'lat.in'
@@ -41,7 +44,8 @@ csInVariables = [
 #                  '@N6BODY':[4,16,64,256,1024],
 #                
 
-clusterlist = [4,16]
+#clusterlist = [4,16]
+clusterlist = [4]
 latInVariables = [
                   ['@N2BODY','lat.in',clusterlist],
                   ['@N3BODY','lat.in',clusterlist],
@@ -61,67 +65,52 @@ toRunList = []
 tools = ceScriptTools.ceTools(mainDir,inputDir,runname,runtype,inputlist,toCheckList,checkedList,toRunList)
 tools.BuildNewRun() #create folders
 
-raw_input("Press enter to submit jobs")
-
-
 import os,subprocess,time,sys, shutil
 
-#newRun = "DOS"
-#newRunFile = "DOSCAR" #Will check to see if higher level is already done, then skip it
-#copyFiles = True
-
-
-print("\nInitializing...\n")
-print("Finding Directories to do %s on\n" % runName)
-dir = mainDir + runType[0] + "/"
-print("Searching all Directories in " + dir+"\n")
-if os.path.exists(dir+'energies'):
-    os.system('cp '+dir+'energies '+ dir+'oldenergies')
+print('Finding Directories to do %s on\n' % runname)
+dir = mainDir + runtype + '/'
+print('Searching all Directories in ' + dir+'\n')
 
 tools.AddToList(dir)
 
-print("Checking to see which folders contain "+runName+"\n")
-time.sleep(1)
+print('Checking to see which folders contain '+runname+'\n')
 tools.CheckFolders()
-checkedList = sorted(checkedList)
-#toRunList=sorted(toRunList)
+#checkedList = sorted(checkedList)
 toRunList=checkedList #run all folders for convergence checks
 
-#copy INCAR back into relax folder
-
-for folder in toRunList:
-    subprocess.call(['cp',incar,folder+'INCAR'])
-#    subprocess.call(['rm',folder+'converged.txt'])
-#    subprocess.call(['ls','-l',folder+'/'+'INCAR']) 
-#    subprocess.call(['cat',folder+'/'+'INCAR'])    
-#modify mixing
-tools.replaceParamIncar(toRunList, 'BMIX', bmix) # may be needed for convergence in isolated atoms
-   
-#print "\nThe following folders are in checkedList:"
-#for i in checkedList:
-#    print("checkedList contains : " + i)
+print '\nThe following folders are in checkedList:'
+for i in checkedList:
+    print('checkedList contains : ' + i)
     
-print "Skipping the following files, isolated atom converged:"
-templist = toRunList[:] #create new list, not just a tag
-for i, path in enumerate(toRunList):
-    if os.path.exists(path+'converged.txt'):
-        templist.remove(path)
-        print 'Already converged', path
-toRunList = templist[:] 
+#print 'Skipping the following files, found finish.txt:'
+#templist = toRunList[:] #create new list, not just a tag
+#for i, path in enumerate(toRunList):
+#    if os.path.exists(path+'finish.txt'):
+#        templist.remove(path)
+#        print 'Already finished', path
+#toRunList = templist[:] 
              
-print "\nThe following folders will be run:"
+print '\nThe following folders will be run:'
 for i in toRunList:
-    print("toRunList contains : " + i)
-
-print"\n"
+    print('toRunList contains : ' + i)
+print'\n'
+jobname = runtype
+outfile = 'out.txt'
+execPath = '/fslhome/bch/cluster_expansion/theuncle/uncle/trunk/uncle.x'
+execLines = ''
+for par in uncleParam:
+    execLines += 'mpiexec '+execPath+' '+str(par)+ ' > ' + outfile + '\n'
+print execLines
 for folder in toRunList:
-    newFolder = folder
-    os.chdir(newFolder)
-    file = open(newFolder+"job",'w+')
-    prefix = 'adatom_'
-    element = newFolder[newFolder.index(prefix)+len(prefix):newFolder.index('/',newFolder.index(prefix))]
-    jobData = "#!/bin/bash\n#PBS -l nodes=1:ppn=1,pmem=2gb,walltime=36:00:00\n#PBS -N " + element+"\n#PBS -m bea\n#PBS -M bret_hess@byu.edu\n# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.\nexport OMP_NUM_THREADS=8\nOUTFILE=\"output.txt\"\n# The following line changes to the directory that you submit your job from\ncd \"$PBS_O_WORKDIR\"\nmpiexec /fslhome/bch/hessgroup/vaspfiles/src/vasp.5.2.12/vasp  > \"$OUTFILE\" \n date > finish.txt \n exit 0"
+    os.chdir(folder)
+    file = open(folder+'job','w+')    
+    jobData = '#!/bin/bash\n#PBS -l nodes=1:ppn=1,pmem=1gb,walltime=1:00:00\n#PBS -N ' + jobname +'\n#PBS -m bea\n#PBS -M bret_hess@byu.edu\n cd \'$PBS_O_WORKDIR\'\n' + execLines + 'date > finish.txt \n exit 0'
     file.write(jobData)
     file.close()
+
+raw_input('Press enter to submit jobs')
+
+for folder in toRunList:
+    os.chdir(folder)
     subprocess.check_call(['qsub','job']) #waits to get response 
-print "Done with submitting jobs"
+print 'Done with submitting jobs'
--- cluster_expansion/run_scripts/ceScriptTools.py
+++ cluster_expansion/run_scripts/ceScriptTools.py
@@ -38,24 +38,18 @@ class ceTools:
         except:
             os.system('mkdir %s' % path1)
             os.chdir(path1)
-        self.DirLoop(self.InputList) 
-        print "Number of run folders created:"
-        print self.DirsCreated
-        
-    def DirLoop(self,inputlist):
         varSets = []
-        for list1 in inputlist:
+        for list1 in self.InputList:
             varSets = varSets + list1  #flatten the inputlist. Each  is of the form  ['@NFITSTRUC','CS.in',[64,128]],  Includes tag and file, vars
         for list1 in varSets:
-            print list1
             inputpath = self.InputDir+list1[1] #file path
             os.system('cp '+ inputpath + ' .')         # copy inputfiles to first level
-        self.DescendOption(varSets)
-        print 'Descend done'
+        self.DescendOption(varSets) 
+        print "Number of run folders created:", self.DirsCreated
+
                         
-    def DescendOption(self,listRemain): #"outer loop" of file structure. Creates run folder if needed
-        listRemain = copy.deepcopy(listRemain)
-        print 'Rem:', len(listRemain),listRemain
+    def DescendOption(self,listRemain):
+        '''Creates dir structure if needed and copies input files into run folders'''
         if len(listRemain) == 0: #done
             if self.RunName in os.listdir(os.getcwd()):
                 print "Already found " + self.RunName + " in folder " + os.getcwd()
@@ -67,12 +61,12 @@ class ceTools:
             return               
         tempList = listRemain[0] #for this level of dirs
         tag = tempList[0]
+        print tag
         tagtext= tag.lower()[1:] #drops the @ symbol and uses only lowercase for dir name
         filename = tempList[1]
         filepath = self.InputDir+'/'+filename
         levelVars = tempList[2]
-        for var in levelVars:   
-            print tagtext, var        
+        for var in levelVars:        
             dirVar = str(tagtext) + '.' + str(var)
             try:
                 os.chdir(dirVar)
@@ -84,7 +78,7 @@ class ceTools:
             except:
                 print 'File system error'
             self.AlterFile(filename,tag,var)
-            self.DescendOption(copy.deepcopy(listRemain[1:]) ) #removes the option that is done, then does recursion.
+            self.DescendOption(listRemain[1:]) #removes the option that is done, then does recursion.
             os.chdir('..')
         try:
                 os.system('rm *.in')
@@ -92,6 +86,7 @@ class ceTools:
             print "File system error\n"                           
 
     def AlterFile(self,filepath,frommatch,tomatch):    
+        '''Substitutes input tags with values'''
         import re
         try:
             fileread = open(filepath,'r')
@@ -107,107 +102,65 @@ class ceTools:
             print "This is likely due to " + frommatch + " tag not being present in the file."
             print tomatch
 
-#    def AlterCsIn(self,option,tag):
-#        curdir = os.getcwd()
-#        dir2 = self.CsInFile+'/'+option+'/'
-#        os.chdir(dir2)        
-#        file=open("CS.in",'r')
-#        file2=file.readlines()
-#        file.close()
-#        file2string=""
-#        for string in file2:
-#            file2string=file2string+string
-#        os.chdir(curdir)
-#        import re
-#        try:
-#            fileread = open('CS.in','r')
-#            text = fileread.read()
-#            fileread.close()
-#            filewrite = open('CS.in','w')
-#            regex = re.compile(str(tag))
-#            text = regex.sub(str(file2string),text)
-#            filewrite.write(text)
-#            filewrite.close()
-#        except:            
-#            print "There was an error in processing file " + dir2+"CS.in"+'/'
-#            print "This is likely due to " + frommatch + " tag not being present in the file."
-#            print tomatch
-#
-#    def FindFolders(self,folder): #Finds all subdirectories
-#        files = os.listdir(folder)
-#        for path in files:
-#            if os.path.isdir(folder+path+"/"):
-#                self.FolderList.append(folder+path+"/")
-#                self.FindFolders(folder+path+"/")
-#    
-#    def CheckFolders(self):
-#        for path in self.ToCheckList:
-#            if path.split("/")[-2] == self.RunName:
-#                self.CheckedList.append(path)
-#                if os.path.exists(path + 'OUTCAR') and self.FinishCheck(path) and self.StepsLessThanNSW(path): 
-#                    print ('Will skip (finished):'+path)    
-#                else:
-#                    self.ToRunList.append(path)             #run only unfinished ones
-#                
-#    def CheckForNewRun(self):
-#        for path in self.CheckedList:
-#            parpath =  os.path.abspath(os.path.join(path, os.path.pardir))
-#            if os.path.exists(os.path.join(parpath,newRun)):
-#                print os.path.join(parpath,newRun) + " already exists."
-#                if copyFiles:
-#                    print "Copying " + newRunFile +" from path to current directory."
-#                    newPath = os.path.join(parpath,newRun) + "/" + newRunFile
-#                    array = parpath.split("/")
-#                    newFileName = array[len(array)-2]+array[len(array)-1]+".dat"
-#                    shutil.copyfile(newPath,mainDir+newFileName)
-#            else:
-#                self.ToRunList.append(parpath+"/")
-#
-#    def FinishCheck(self,folder):
-#        """Tests whether Vasp is done by finding "Voluntary" in last line of OUTCAR."""
-#        lastfolder = os.getcwd()
-#        os.chdir(folder)
-#        proc = subprocess.Popen(['grep', 'Voluntary', 'OUTCAR'],stdout=subprocess.PIPE)
-#        newstring = proc.communicate()
-#        os.chdir(lastfolder)    
-#        return newstring[0].find('Voluntary') > -1 #True/False
-#    
-#    def StepsLessThanNSW(self,folder):
-#        '''Get number of steps in relaxation, and check vs NSW'''
-#        #get number of steps
-#        lastfolder = os.getcwd()
-#        os.chdir(folder)
-#        if not os.path.exists('OSZICAR') or os.path.getsize('OSZICAR') == 0: 
-#            os.chdir(lastfolder) 
-#            steps = -9999
-#        oszicar = open('OSZICAR','r')
-#        laststep = oszicar.readlines()[-1].split()[0]
-#        oszicar.close()
-#        os.chdir(lastfolder)  
-#        try:
-#            value = int(laststep)
-#            steps =  value
-#        except:
-#            steps =  9999
-#        #check vs NSW
-#        proc = subprocess.Popen(['grep','-i','NSW',self.CheckedList[0]+'/INCAR'],stdout=subprocess.PIPE)
-#        NSW = int(proc.communicate()[0].split('=')[-1])
-#        return steps < NSW # True/False      
-#
-#    def replaceParamIncar(self,pathlist, param, value):
-#        """Replaces a parameter value in Incar with a new value"""
-#        lastfolder = os.getcwd()
-#        for path in pathlist:
-#                os.chdir(path)
-#                incarfile = open('INCAR', 'r')
-#                lines = incarfile.readlines()
-#                incarfile.close()                
-#                incarfile = open('INCAR', 'w')                
-#                for line in lines:
-#                    line2 = line         
-#                    if param in line2:
-#                        line2 = param + ' = ' + value + '\n'
-#                    incarfile.writelines(line2)
-#                incarfile.close()
-#        os.chdir(lastfolder)    
-#        return
+
+
+    def FindFolders(self,folder): #Finds all subdirectories
+        files = os.listdir(folder)
+        for path in files:
+            if os.path.isdir(folder+path+"/"):
+                self.FolderList.append(folder+path+"/")
+                self.FindFolders(folder+path+"/")
+    
+    def CheckFolders(self):
+        for path in self.ToCheckList:
+            if path.split("/")[-2] == self.RunName:
+                self.CheckedList.append(path)
+                if os.path.exists(path + 'OUTCAR') and self.FinishCheck(path) and self.StepsLessThanNSW(path): 
+                    print ('Will skip (finished):'+path)    
+                else:
+                    self.ToRunList.append(path)             #run only unfinished ones
+                
+    def CheckForNewRun(self):
+        for path in self.CheckedList:
+            parpath =  os.path.abspath(os.path.join(path, os.path.pardir))
+            if os.path.exists(os.path.join(parpath,newRun)):
+                print os.path.join(parpath,newRun) + " already exists."
+                if copyFiles:
+                    print "Copying " + newRunFile +" from path to current directory."
+                    newPath = os.path.join(parpath,newRun) + "/" + newRunFile
+                    array = parpath.split("/")
+                    newFileName = array[len(array)-2]+array[len(array)-1]+".dat"
+                    shutil.copyfile(newPath,mainDir+newFileName)
+            else:
+                self.ToRunList.append(parpath+"/")
+
+    def FinishCheck(self,folder):
+        """Tests whether Vasp is done by finding "Voluntary" in last line of OUTCAR."""
+        lastfolder = os.getcwd()
+        os.chdir(folder)
+        proc = subprocess.Popen(['grep', 'Voluntary', 'OUTCAR'],stdout=subprocess.PIPE)
+        newstring = proc.communicate()
+        os.chdir(lastfolder)    
+        return newstring[0].find('Voluntary') > -1 #True/False
+    
+    def StepsLessThanNSW(self,folder):
+        '''Get number of steps in relaxation, and check vs NSW'''
+        #get number of steps
+        lastfolder = os.getcwd()
+        os.chdir(folder)
+        if not os.path.exists('OSZICAR') or os.path.getsize('OSZICAR') == 0: 
+            os.chdir(lastfolder) 
+            steps = -9999
+        oszicar = open('OSZICAR','r')
+        laststep = oszicar.readlines()[-1].split()[0]
+        oszicar.close()
+        os.chdir(lastfolder)  
+        try:
+            value = int(laststep)
+            steps =  value
+        except:
+            steps =  9999
+        #check vs NSW
+        proc = subprocess.Popen(['grep','-i','NSW',self.CheckedList[0]+'/INCAR'],stdout=subprocess.PIPE)
+        NSW = int(proc.communicate()[0].split('=')[-1])
+        return steps < NSW # True/False      
