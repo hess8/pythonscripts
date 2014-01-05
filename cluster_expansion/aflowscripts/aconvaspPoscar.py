7afe77db72282b2f4cd868966dd7696debbea48e
Bret Hess <hess.byu@gmail.com>
2014-01-04 22:06:00 -0700

    added aconvaspPoscar, to create poscar directly from structure number

---
+++ cluster_expansion/aflowscripts/aconvaspPoscar.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+''' For each structure dir in maindir, add POSCAR using aconvasp.  dir can be empty  '''
+    
+import sys,os
+
+maindir = '/fslhome/bch/cluster_expansion/alir/AFLOWDATAf1_50e/AlIr/'
+atomic = 'Al:Ir'
+os.chdir(maindir)
+dirs= sorted([d for d in os.listdir(os.getcwd()) if os.path.isdir(d)])
+print dirs
+for dir in dirs:
+    os.chdir(dir)
+    commstr2= 'aconvasp --proto=%s:%s | aconvasp --poscar>POSCAR' % (dir,atomic)   #for POSCAR creation         
+    os.system(commstr2)
+    os.chdir(maindir)
+
+
+
+################## functions #######################
+#def readTrainStruc(dir,filename):
+#    os.chdir(dir)
+#    file1 = open(filename,'r')
+#    lines = file1.readlines()
+#    file1.close() 
+#    structs = [line.split()[1] for line in lines] #2nd column
+#    return structs
+#
+#def aflowCreateJobs(structs,atomic,finalDir):
+#    nsubmit = 1 #number of structures to submit in each execution of aflow
+#    for i in range(len(structs)/nsubmit+1):
+#        if (i+1)*nsubmit < len(structs)-1:
+#            sublist = structs[i*nsubmit:(i+1)*nsubmit]
+#        else:
+#            sublist = structs[i*nsubmit:]
+#        strucstr = ''
+#        for struc in sublist:
+##            strucstr += 'f%s,'% struc 
+#            print struc  
+#            strucstr += 'f%s'% struc #note the comma, and long string of structures in 'f%s,' doesn't work for '--proto='    
+##            strucstr += 'h%s,'% struc
+#            
+#        try:
+##            'aconvasp --proto=f1:Ag:Zr | aconvasp --poscar>POSCAR'
+##            commstr= 'aconvasp --aflow_proto ' + strucstr+ ':%s' % atomic 
+#            commstr2= 'aconvasp --proto=%s:%s | aconvasp --poscar>POSCAR' % (strucstr,atomic)   #for POSCAR creation         
+##            commstr= 'aconvasp --aflow_proto ' + strucstr+ ':%s' % atomic
+##            os.system(commstr)
+#            print commstr2
+#
+#            os.system(commstr2)
+#        
+#        except:
+#            print 'Error in executing %s' % commstr
+#    os.system('cp -r AFLOWDATA/* %s' % finalDir)
+#    os.system('rm -r AFLOWDATA/')
+#
+#def otherPrep():
+##    os.system("perl -pi -e 's/KPPRA=6000/KPPRA=10000/' */*/*/aflow.in")
+##    os.system("perl -pi -e 's/SYM=ON/SYM=OFF/' */*/*/aflow.in")
+#    os.system("find `pwd` -name 'aflow.in' > jobs2run")
+#    
+################## script #######################
+##filename='training_set_structures.dat'
+##filename='f11000.dat'
+#filename='f1_50.dat'
+#mainDir = '/fslhome/bch/cluster_expansion/alir/'
+##mainDir = '/fslhome/bch/cluster_expansion/hexagonal/'
+##finalDir = '/fslhome/bch/cluster_expansion/alir/AFLOWDATA500/'
+##finalDir = '/fslhome/bch/cluster_expansion/alir/h1_50/'
+#finalDir = '/fslhome/bch/cluster_expansion/alir/f1_50b/'
+#if not os.path.isdir(finalDir):
+#    os.system('mkdir %s' % finalDir)
+#atomic = 'Al:Ir'
+#structs = readTrainStruc(mainDir,filename)
+#print structs
+#aflowCreateJobs(structs,atomic,finalDir)
+#os.chdir(finalDir)
+#otherPrep()
+print 'Done'
--- cluster_expansion/aflowscripts/train_structs2aflow.py
+++ cluster_expansion/aflowscripts/train_structs2aflow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-''' Reads structure numbers from training_set_structures.dat, 
-    and creates directories with Aflow.in files. '''
+''' Reads structure numbers from training_set_structures.dat,
+and creates directories with Aflow.in files. '''
     
 import sys,os
 ################# functions #######################
@@ -8,7 +8,7 @@ def readTrainStruc(dir,filename):
     os.chdir(dir)
     file1 = open(filename,'r')
     lines = file1.readlines()
-    file1.close() 
+    file1.close()
     structs = [line.split()[1] for line in lines] #2nd column
     return structs
 
@@ -21,34 +21,28 @@ def aflowCreateJobs(structs,atomic,finalDir):
             sublist = structs[i*nsubmit:]
         strucstr = ''
         for struc in sublist:
+# strucstr += 'f%s,'% struc
             strucstr += 'f%s,'% struc
-#            strucstr += 'h%s,'% struc
-            
         try:
-            'aconvasp --proto=f1:Ag:Zr | aconvasp --poscar>POSCAR'
-#            commstr= 'aconvasp --aflow_proto ' + strucstr+ ':%s' % atomic 
-            commstr2= 'aconvasp --proto=%s:%s | aconvasp --poscar>POSCAR' % (strucstr,atomic)   #for POSCAR creation         
-#            commstr= 'aconvasp --aflow_proto ' + strucstr+ ':%s' % atomic
+            commstr= 'aconvasp --aflow_proto ' + strucstr+ ':%s' % atomic
             os.system(commstr)
-            os.system(commstr2)
         except:
             print 'Error in executing %s' % commstr
     os.system('cp -r AFLOWDATA/* %s' % finalDir)
     os.system('rm -r AFLOWDATA/')
 
 def otherPrep():
-#    os.system("perl -pi -e 's/KPPRA=6000/KPPRA=10000/' */*/*/aflow.in")
-#    os.system("perl -pi -e 's/SYM=ON/SYM=OFF/' */*/*/aflow.in")
+# os.system("perl -pi -e 's/KPPRA=6000/KPPRA=10000/' */*/*/aflow.in")
+# os.system("perl -pi -e 's/SYM=ON/SYM=OFF/' */*/*/aflow.in")
     os.system("find `pwd` -name 'aflow.in' > jobs2run")
     
 ################# script #######################
 #filename='training_set_structures.dat'
 #filename='f11000.dat'
-filename='h1_50.dat'
+filename='f1_50.dat'
 mainDir = '/fslhome/bch/cluster_expansion/alir/'
-#mainDir = '/fslhome/bch/cluster_expansion/hexagonal/'
 #finalDir = '/fslhome/bch/cluster_expansion/alir/AFLOWDATA500/'
-finalDir = '/fslhome/bch/cluster_expansion/alir/h1_50b/'
+finalDir = '/fslhome/bch/cluster_expansion/alir/AFLOWDATAf1_50e/'
 if not os.path.isdir(finalDir):
     os.system('mkdir %s' % finalDir)
 atomic = 'Al:Ir'
