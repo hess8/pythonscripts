fe7b0cf3d0d10de8dce8c9ac349860b1846ea7ec
Bret Hess <hess.byu@gmail.com>
2014-09-12 16:34:23 -0600

    Fixed structures weighting

--- cluster_expansion/ceflashscripts/weight_structures_in.py
+++ cluster_expansion/ceflashscripts/weight_structures_in.py
@@ -26,13 +26,16 @@ from analysisToolsVasp import writeEnergiesOszicar, writedirnames, nstrip, write
   getms, writefermi, removezeros,getEf
 
 def weightfunc(FE,FEmax,FEmin):
-    return str(1 + 10*(FEmax-FE)/(FEmax-FEmin))
+    maxratio = 1.0
+    return str(1.0 + (maxratio-1)*(FEmax-FE)/(FEmax-FEmin))
 
 '''Reads formation energies in structures.in.  If weighted, it changes the weights.  
 If not, it adds a line for each weight'''
 
-dir = '/fslhome/bch/cluster_expansion/graphene/csv1-8W/'
+#dir = '/fslhome/bch/cluster_expansion/graphene/csv1-8W/'
+dir = '/fslhome/bch/cluster_expansion/graphene/test1/'
 infile = dir+'structures.in.441.FEsorted'
+#infile = dir+'structures.in'
 outfile = dir+'structures.in'
 FEs = []
 indices = []
@@ -45,22 +48,22 @@ else:
 #print 'weights_in', weights_in
 #Find maximum and min formation energies
 for i, line in enumerate(lines):
-    if '#Energy' in line:
-        energy = float(lines[i+1].strip())
+    if 'FE' in line:
+        energy = float(line.split()[line.split().index('FE')+2].split(',')[0])  #FE = 1.2578,  etc.  remove any ","
         FEs.append(energy)
+    if '#Energy' in line:
         indices.append(i+2) #where weights should appear 
 FEmax = amax(FEs)
 FEmin = amin(FEs)
 indices = array(indices)
 
-
 #Write weights
 for i in range(len(FEs)):
     if weights_in:
-        lines[indices[i-1]] = '#Energy and weight'
-        lines[indices[i]] = str(weightfunc(energy,FEmax,FEmin))+'\n'
+        lines[indices[i]-2] = '#Energy and weight'+'\n'
+        lines[indices[i]] = str(weightfunc(FEs[i],FEmax,FEmin))+'\n'
     else: #have to add lines
-        lines.insert(indices[i],weightfunc(FEs[i],FEmax,FEmin)+'\n')
+        lines.insert(indices[i],str(weightfunc(FEs[i],FEmax,FEmin))+'\n')
         indices += 1 #increment all indices by one to handle new lines
 writefile(lines,outfile)
 
