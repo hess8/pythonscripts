391d82af01586e4f495e8952b859caf7beaf0e82
Bret Hess <hess.byu@gmail.com>
2014-10-25 10:18:32 -0600

    Starting on DOS creation

--- cluster_expansion/analysis_scripts/procar_bch.py
+++ cluster_expansion/analysis_scripts/procar_bch.py
@@ -7,6 +7,7 @@ sys.path.append('/bluehome2/bch/pythonscripts/cluster_expansion/analysis_scripts
 from numpy import zeros,transpose,array,sum,float64,rint,mean,set_printoptions,s_,\
     delete,append,cross,dot
 from numpy.linalg import norm
+#from numpy.ndarray import flatten
 from analysisToolsVasp import writeEnergiesOszicar, writedirnames, nstrip, writeNk, writeNkIBZ, \
   writeElConverge, writeElSteps, writeCPUtime, enerparts, getdata, readfile, writefile, \
   getms, writefermi, removezeros
@@ -31,6 +32,7 @@ class procar:
         self.occ = []
         
     def incar(self,dir):
+        print 'reading ISPIN and LORBIT from INCAR'
         lines = nstrip(readfile(dir+'INCAR'))
         for line in lines:
             if ('ISPIN' in line) or ('ispin' in line): self.spin = int(line.split('=')[1])
@@ -45,6 +47,7 @@ class procar:
             self.norbs = len(self.orbs)              
         
     def read(self,filepath):
+        print 'reading Procar',
         lines = nstrip(readfile(filepath))
         self.nk = int(lines[1].split(':')[1].split()[0])
         self.nbands = int(lines[1].split(':')[2].split()[0])
@@ -69,111 +72,34 @@ class procar:
                     self.occ[ik,ib] = float(lines[ipos].split()[7])
                     ipos += 3
                     for i in range(self.nions):
-                        print ipos
+                        if mod(ipos,1e4)==0: print '-', #progress bar
                         tempw = lines[ipos].split()[1:self.norbs+1]
-                        print tempw
                         self.weights[ik,ib,:,ispin] = tempw
                         ipos += 1
                     ipos += 2
                 ipos += 1
-                                                                                             
+        print       
+        
+class dos:
+    def __init__(self):     
+        '''dE is width of gaussian added for each eigenvalue'''
+    
+    
+    def calc(self,pcr):
+        emin = amin(pcr.ener.flatten()) 
+        emax = amax(pcr.ener.flatten()) 
+        print emin, emax
+#        dosarr = zeros()                                                                 
 ################# script #######################
 #maindir = '/fslhome/bch/cluster_expansion/alal/test/f1_2/'
-maindir = '/fslhome/bch/cluster_expansion/graphene/analysis/1220/DOS/'
-prcr = procar()
-prcr.incar(maindir)
-prcr.read(maindir+'PROCAR')
+#maindir = '/fslhome/bch/cluster_expansion/graphene/analysis/1220/DOS/'
+maindir = '/fslhome/bch/cluster_expansion/graphene/analysis/1220/DOSlorbit11/'
+pcr = procar();dos = dos()
+pcr.incar(maindir)
+pcr.read(maindir+'PROCAR')
 
-#wrtvasp = write_vasp()
-#q = query()
-#
-#set_printoptions(precision=12)
-#os.chdir(maindir)
-#dir = '1init/'
-#tet.get_tetweights(dir)
-#
-#kp.get_eigen(dir)
-#q.ener(kp)
-#tet.get_tetvecs(dir)
-##q.tetwgt(tet,kp) #prints
-#
-##print 'tet8', sum(tet.fwgt[8,:]);tet8=sum(tet.fwgt[8,:])
-#print 'tet vecs'
-#print tet.vecs
-#tet.rd_ibzkpt(dir)
-#print 'tet ids to kpoints'
-#kp.twgt2kwgt(tet) #kpweights from tetweights
-#print tet.idk
-#print 'tet volume weights'
-#print tet.vwgt
-#print 'Sum volume weights', sum(tet.vwgt)
-#print 'tet volumes'
-#print tet.vol
-#kp.rd_ibzkpt(dir)
-#print 'kpoint vecs'
-#print kp.vecs
-#print 'nk',kp.nk
-#kp.make_ids(tet)
-#print 'kpoint connection to tets'
-#print kp.idt
-#
-#sumfwgt1 = q.sum_fermi(tet,kp)
-#add_tets(tet,kp)
-#
-#sumfwgt2 = q.sum_fermi(tet,kp)
-#kp.twgt2kwgt(tet) #kpweights from tetweights
-##print 'tet weights'
-##print tet.ntet,tet.fwgt
-##print 'Energies'
-##print kp.ener
-##print 'tet vecs'
-##print tet.vecs
-##print 'tet volume weights'
-##print tet.vwgt
-##print 'tet volumes'
-##print tet.vol
-##print 'tet ids to kpoints'
-##print tet.idk
-##print 'kpoint vecs'
-##print kp.vecs
-##print 'kpoint to tet ids'
-##kp.make_ids(tet)
-##print kp.idt
-##print 'nk',kp.nk
-##print 'ntet',tet.ntet
-##print 'Sum volume weights', sum(tet.vwgt)
-#
-##### VASP run with added k's
-#dir = '2addk/'
-#wrtvasp.kpfile(tet,kp,dir)  
-##run vasp !!
-#
-#print 'Get new eigenvals, weights, but not vecs'
-#tet.get_tetweights(dir)
-#
-#kp.twgt2kwgt(tet) #kpweights from tetweights
-#kp.get_eigen(dir)
-#q.ener(kp)
-#
-#sumfwgt3 = q.sum_fermi(tet,kp)
-#
-#
-##print 'tet 8old', tet8
-##print 'tet 26-29' ,sum(tet.fwgt[26:30,:])
-##print sumfwgt3
-#if abs(sumfwgt3-sumfwgt1)>1e-7: print '  SOMEHOW LOSING FERMI WEIGHT IN VASP!'    
-#
-##### Add more kpoints
-###weights are getting below integers:
-##
-#add_tets(tet,kp)
-#tet.volscale = tet.volscale/4.0
-#tet.vwgt = tet.vwgt*4.0
-##
-###### VASP run with added k's
-#dir = '3addk/'
-#wrtvasp.kpfile(tet,kp,dir)   
-#
-#
-#
-#print 'Done'
+#DOS
+dos.calc(pcr)
+
+print 'Done'
+
