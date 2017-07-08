2954477ad2413cfb98b969fcf5c8b565ac8e1ad2
Bret Hess <hess.byu@gmail.com>
2017-07-08 09:20:59 -0600

    small changes

--- cluster_expansion/ceflashscripts/submitVaspKmesh_VC.py
+++ cluster_expansion/ceflashscripts/submitVaspKmesh_VC.py
@@ -12,11 +12,11 @@ from numpy.linalg import norm
 sys.path.append('/bluehome2/bch/pythonscripts/cluster_expansion/ceflashscripts/symmetry_k_mesh_search')
 #import kmeshroutines as km
 from kmeshroutines import nstrip, readposcar,create_poscar
-import dynamicPacking2
+import dynamicPackingtest
 
 def getVCmesh(dir,method,targetNmesh,meshtype):
     lastDir = os.getcwd()   
-    meshc = dynamicPacking2.dynamicPack() #instance
+    meshc = dynamicPackingtest.dynamicPack() #instance
     [descriptor, scale, latticevecs, reciplatt, natoms, postype, positions] = readposcar('POSCAR',dir)
 #         create_poscar('POSCAR',descriptor, scale, latticevecs, natoms, postype, positions, path) #just to remove the scale problem
     os.chdir(dir)
