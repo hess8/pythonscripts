825fc6884a9dc42337a50c50e7b99e1e8fdbc833
Bret Hess <hess.byu@gmail.com>
2017-07-20 09:50:46 -0600

    added vaspoutCombineRunsExternalData version to handle Wileys data

---
+++ cluster_expansion/analysis_scripts/vaspoutCombineRunsExternalData.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+'''
+Comparison plot for different k mesh methods
+'''
+
+import sys,os,subprocess
+from numpy import zeros,transpose,array,sum,float64,rint,mean,sort,argsort
+from numpy.linalg import norm
+from analysisToolsVasp import getEnergy, getNkIBZ,getNatoms, readfile, writefile,electronicConvergeFinish
+sys.path.append('/bluehome2/bch/pythonscripts/cluster_expansion/analysis_scripts/plotting/') 
+sys.path.append('/bluehome2/bch/pythonscripts/cluster_expansion/ceflashscripts')
+from symmetry import get_lattice_pointGroup, get_spaceGroup #these have vectors as ROWS
+from kmeshroutines import readposcar
+
+# from plotTools import plotxy
+from pylab import figure,cm,plot,xlabel,ylabel,title,loglog,semilogy,legend
+from copy import deepcopy
+
+def areEqual(x,y,eps):
+    return abs(x-y)<eps
+
+testfile = 'POSCAR'
+
+# paths = ['/fslhome/bch/cluster_expansion/mpmesh/cu.pt.ntest/12fstrEK_fcc'
+#         ,'/fslhome/bch/cluster_expansion/vcmesh/the99']
+
+paths = ['/fslhome/bch/cluster_expansion/vcmesh/the99sym','/fslhome/bch/cluster_expansion/mpmesh/mpPure']
+# paths = ['/fslhome/bch/cluster_expansion/vcmesh/semicond','/fslhome/bch/cluster_expansion/mpmesh/semicond']
+# paths = ['/fslhome/bch/cluster_expansion/vcmesh/test','/fslhome/bch/cluster_expansion/mpmesh/semicond']
+extpath = '/fslhome/bch/cluster_expansion/vcmesh/mueller_mp_data'
+
+filter = 'Cu' #string must be in dir name to be included
+summaryPath = paths[0]
+# summaryPath = '/fslhome/bch/cluster_expansion/vcmesh/cu17Jul17/'
+# summaryPath = paths[1]
+iplot = 0
+maxCalcs = 0
+#count the number of plots:
+for ipath,maindir in enumerate(paths):
+    os.chdir(maindir)
+    structs = sorted([d for d in os.listdir(os.getcwd()) if os.path.isdir(d) and filter in d])
+    for struct in structs:
+        os.chdir(struct)
+        iplot += 1
+        calcs = sorted([d for d in os.listdir(os.getcwd()) if os.path.isdir(d)])
+        if len(calcs)>maxCalcs: maxCalcs = len(calcs)
+        os.chdir(maindir)
+
+#external data is of the form extpath/atom_method/struct.csv.  The csv has energies vs nK
+os.chdir(extpath)
+atoms_methods = sorted([d for d in os.listdir(extpath) if os.path.isdir(d) and filter in d])# os.chdir(extpath)
+for atom_method in atoms_methods:
+    os.chdir(atom_method)
+    os.system('rm -r .*lock*')
+    for struct in os.listdir(os.getcwd()):
+        iplot += 1
+        #count number of points in this struct
+        lines = readfile(struct)
+        if len(lines)>maxCalcs: maxCalcs = len(lines)     
+    os.chdir(extpath)
+
+nplots = iplot 
+if nplots < len(paths): sys.exit('Stop.  Not enough structures match filter')      
+data = zeros(nplots,dtype = [('ID', 'S15'),('nDone','int8'),('nAtoms','int8'),('nops','int8'),('eners', '{}float'.format(maxCalcs)),\
+                ('errs', '{}float'.format(maxCalcs)),('nKs', '{}int16'.format(maxCalcs))])
+#read all the data 
+iplot = -1
+for ipath, maindir in enumerate(paths):
+#     meshMethod = maindir.split('/')[-3][:3]+maindir.split('/')[-1][-3:]
+    meshMethod = maindir.split('/')[-1][-7:]
+    os.chdir(maindir)
+    structs = sorted([d for d in os.listdir(os.getcwd()) if os.path.isdir(d) and filter in d])
+#     structs = ['f9292']; print'only struct 9292'
+    nStructs = len(structs)
+    print;print structs,maindir
+    for istruct,struct in enumerate(structs):
+#         print 'test', istruct, struct
+        iplot += 1
+        os.chdir(struct)
+        calcs = sorted([d for d in os.listdir(os.getcwd()) if os.path.isdir(d) and os.path.exists('{}/OUTCAR'.format(d))])
+        energies = []
+        nKs = []
+        nAtoms = []
+        nDone = 0
+        for calc in calcs:  
+#             print 'calc',calc
+            if electronicConvergeFinish(calc):
+                ener = getEnergy(calc) #in energy/atom
+                if not areEqual(ener,0,1e-5):
+                    nDone +=1
+                    energies.append(ener)
+                    if 'vc' in maindir:
+                        nK = getNkIBZ(calc,'KPOINTS')
+                    else:
+                        nK = getNkIBZ(calc,'IBZKPT')
+                    nKs.append(nK)
+        #sort by increasing number of kpoints
+        if len(energies)>0: 
+            nKs = array(nKs)
+            energies = array(energies)
+            order = argsort(nKs)
+    #         print 'struct',struct
+    #         print 'energies',energies
+            energies = energies[order]
+            eref = energies[-1]#the last energy of each struct is that of the most kpoints
+    #         print 'eref',eref
+    #         print 'energies sorted',energies, 'nKs', nKs
+    #         print
+            nKs = sort(nKs)
+    #         print 'NKs', nKs      
+    #         if ipath == 0 and istruct == 0: ******* useful if comparing methods on the same struct with a trusted first method*******
+    #             eref = energies[-1] #the last energy of each struct is that of the most kpoints
+    
+            errs = abs(energies-eref)*1000 + 1e-6 #now in meV 
+    #         plotErrs()
+            data[iplot]['ID'] = '{} {}'.format(struct,meshMethod)
+            nAtoms = getNatoms('{}/POSCAR'.format(calc))
+            data[iplot]['nAtoms'] = nAtoms
+            data[iplot]['nDone'] = nDone
+            data[iplot]['eners'][:nDone] = energies
+            data[iplot]['errs'][:nDone] = errs
+            data[iplot]['nKs'][:nDone] = nKs
+        os.chdir(maindir)
+os.chdir(extpath)
+
+
+for atom_method in atoms_methods:
+    os.chdir(atom_method)
+    for structfile in os.listdir(os.getcwd()):
+        iplot += 1
+        energies = []
+        nKs = []
+        lines = readfile(structfile)
+        for line in lines:
+            nKs.append(int(line.split('\t')[0]))
+            energies.append(-float(line.split('\t')[1].split('\r')[0]))
+        nKs = array(nKs)
+        energies = array(energies)
+        nDone = len(energies)
+        order = argsort(nKs)
+        energies = energies[order]
+        eref = energies[-1]#the last energy of each struct is that of the most kpoints
+        nKs = sort(nKs)
+        errs = abs(energies-eref)*1000 + 1e-6 #now in meV 
+        struct = structfile.split('_')[0]
+        data[iplot]['ID'] = atom_method + struct
+        data[iplot]['nAtoms'] = 0
+        data[iplot]['nDone'] = len(energies)
+        data[iplot]['eners'][:nDone] = energies
+        data[iplot]['errs'][:nDone] = errs
+        data[iplot]['nKs'][:nDone] = nKs
+    os.chdir(extpath)
+
+lines = [' ID , nKIBZ , ener , err, nAtoms \n']  
+for iplot in range(nplots):
+    n = data[iplot]['nDone']
+    for icalc in range(n):#data[iplot]['eners'][:n].tolist()
+        lines.append('{},{},{:15.12f},{:15.12f},{}\n'.format(data[iplot]['ID'], data[iplot]['nKs'][icalc],\
+             data[iplot]['eners'][icalc],data[iplot]['errs'][icalc],data[iplot]['nAtoms']))  
+writefile(lines,'{}/summary.csv'.format(summaryPath))   
+fig = figure()
+ax1 = fig.add_subplot(111)
+#    ax1.set_color_cycle(['r','b','g','c', 'm', 'y', 'k'])
+xlabel('N kpoints')
+ylabel('Vasp energy/atom (eV)') 
+title('Convergence vs mesh method')
+#ylim((1e-12,1e0))
+for iplot in range(nplots):
+    if iplot < nplots -1:
+        plotcolor = cm.jet(1.*(iplot+1)/float(nplots))
+    else:
+        plotcolor = 'k'
+    n = data[iplot]['nDone']  
+#     print 'iplot',data[iplot]['eners'][:n], data[iplot]['nKs'][:n]
+    plot(data[iplot]['nKs'][:n],data[iplot]['eners'][:n],\
+      label=data[iplot]['ID'],linestyle='None',color = plotcolor, marker = 'o',markeredgewidth=0.0) 
+legend(loc='lower left',prop={'size':12});
+# show()
+fig.savefig('{}/energy_vs_n'.format(summaryPath))         
+        
+fig = figure()
+ax1 = fig.add_subplot(111)
+#    ax1.set_color_cycle(['r','b','g','c', 'm', 'y', 'k'])
+xlabel('N kpoints')
+ylabel('Error (meV)') 
+title('Convergence vs mesh method')
+#ylim((1e-12,1e0))
+for iplot in range(nplots):
+    if iplot < nplots -1:
+        plotcolor = cm.jet(1.*(iplot+1)/float(nplots))
+    else:
+        plotcolor = 'k'
+    n = data[iplot]['nDone']  
+    semilogy(data[iplot]['nKs'][:n],data[iplot]['errs'][:n],\
+      label=data[iplot]['ID'],linestyle='None',color=plotcolor, marker = 'o',markeredgewidth=0.0) 
+legend(loc='lower left',prop={'size':12});
+# show()
+fig.savefig('{}/log_err_vs_n'.format(summaryPath)) 
+
+#log-log
+fig = figure()
+ax1 = fig.add_subplot(111)
+#    ax1.set_color_cycle(['r','b','g','c', 'm', 'y', 'k'])
+xlabel('N kpoints')
+ylabel('Error (meV)') 
+title('Convergence vs mesh method')
+#ylim((1e-12,1e0))
+for iplot in range(nplots):
+    if iplot < nplots -1:
+        plotcolor = cm.jet(1.*(iplot+1)/float(nplots))
+    else:
+        plotcolor = 'k'
+    n = data[iplot]['nDone']  
+    ax1.loglog(data[iplot]['nKs'][:n],data[iplot]['errs'][:n],\
+      label=data[iplot]['ID'],linestyle='None',color=plotcolor, marker = 'o',markeredgewidth=0.0) 
+legend(loc='lower left',prop={'size':12});
+# show()
+fig.savefig('{}/loglog_err_vs_n'.format(summaryPath)) 
+
+
+print 'Done'
+
