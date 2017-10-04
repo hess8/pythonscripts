c40bfadc359a31992884b507e7d16e3e5a36328a
Bret Hess <hess.byu@gmail.com>
2017-10-03 18:34:42 -0600

    Si test results: 3x3 search: 1.47, 16 done full search: 1.75, 16 done     params0 =     [ 0.5 ]   #wallClose     params1 =     [ 1 ]   #useVoids     params2 =     [ 3.0 ]   #rcutoff     params3 =     [ 1.0 ]   #tooClose     params4 =     [ 0.5 ]  #tooPlanar     params5 =     [ 8   ]  #NvoidClosePoints     params6 =     [ 3.0 ]  #vwPower     params7 =     [ 3.0 ]   #wallPower     params8 =     [ 1 ]   #relax (boolean)     params9 =     [ 6.0 ]  #interPower     params10 =     [ 0.5 ]  #wallFactor     params11 =    [ 0.0 ]  #wallOffset

--- cluster_expansion/ceflashscripts/symmetry_k_mesh_search/voidWeightingRelax.py
+++ cluster_expansion/ceflashscripts/symmetry_k_mesh_search/voidWeightingRelax.py
@@ -957,15 +957,15 @@ class voidWeight():
         '''Test an entire grid of init values'''
         cubicLVs0 = cubicLVs
         nShift = 5
-# #         
+          
 #         nTh = 10
 #         nPh = 20
-        
+#         
         print '!!!!!!!!!!!!!!Using only 3x3 angle search!!!!!!!!!!!!!!' 
         print '!!!!!!!!!!!!!!Using only 3x3 angle search!!!!!!!!!!!!!!'             
         nTh = 3
         nPh = 3
-# 
+ 
 
 
         shiftDiv = 0.5*sqrt(3)/float(nShift)
